34935339df04c1b41e1dd9f1769bac040915c666
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-02-27 13:03:23 +0100

    fixed media player related bugs

--- src/components/players/audioPlayer.jsx
+++ src/components/players/audioPlayer.jsx
@@ -8,36 +8,36 @@ export function AudioPlayer({ src }) {
     const controlsRef = useRef();
     const intervalRef = useRef();
 
-    const [playing, setPlaying] = useState(false);
-    const [duration, setDuration] = useState(0);
-    const [current, setCurrent] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
     const [isMuted, setIsMuted] = useState(false);
+    const [currentTime, setCurrentTime] = useState(Number);
+
     const [source, setSource] = useState(src[1]);
+    const [metadata, setMetadata] = useState(false);
 
     function play() {
         if(audioRef.current.paused) {
             audioRef.current.play();
-            setPlaying(true);
-            setDuration(audioRef.current.duration);
+            setIsPlaying(true);
             intervalRef.current = setInterval(() => {
                 updateTime();
             }, 100);
         } else {
             audioRef.current.pause();
-            setPlaying(false);
+            setIsPlaying(false);
             clearInterval(intervalRef.current);
         };
     };
 
     function updateTime() {
         seekerRef.current.value = (audioRef.current.currentTime / audioRef.current.duration) * 100;
-        setCurrent(audioRef.current.currentTime);
+        setCurrentTime(audioRef.current.currentTime);
     };
 
     function seek() {
         if(audioRef.current.duration) {
             audioRef.current.currentTime = audioRef.current.duration * (seekerRef.current.value / 100);
-            setCurrent(audioRef.current.currentTime);
+            setCurrentTime(audioRef.current.currentTime);
         };
     };
 
@@ -59,15 +59,17 @@ export function AudioPlayer({ src }) {
 
     function next() {
         if((src[2].indexOf(source) + 1) < src[2].length) {   
-            clearInterval(intervalRef.current);
-            setPlaying(false);    
             setSource(src[2][src[2].indexOf(source) + 1]);
+            clearInterval(intervalRef.current);
+            setMetadata(false);
+            setIsPlaying(false);
+            setCurrentTime(0);
+            audioRef.current.pause();
             seekerRef.current.value = 0;
-            setCurrent(0);
         };
     };
 
-    function autoPlay() {
+    async function autoPlay() {
         if((src[2].indexOf(source) + 1) < src[2].length) {   
             setSource(src[2][src[2].indexOf(source) + 1]);
 
@@ -78,50 +80,51 @@ export function AudioPlayer({ src }) {
             }, 0);
 
             seekerRef.current.value = 0;
-            setCurrent(0);
+            setCurrentTime(0);
         } else if((src[2].indexOf(source) + 1) >= src[2].length) {
             clearInterval(intervalRef.current);
-            setPlaying(false);
         };
     };
 
     function prev() {
         if((src[2].indexOf(source) - 1) >= 0) {
-            clearInterval(intervalRef.current);
-            setPlaying(false);    
             setSource(src[2][src[2].indexOf(source) - 1]);
+            clearInterval(intervalRef.current);
+            setMetadata(false);
+            setIsPlaying(false);
+            setCurrentTime(0);
+            audioRef.current.pause();
             seekerRef.current.value = 0;
-            setCurrent(0);
         };
     };
-
+ 
     useEffect(() => {
-        seekerRef.current.value = 0;
-        volumeRef.current.value = 0.3;
         return () => clearInterval(intervalRef.current);
     }, []);
     
     return (
         <section className="h-screen w-full flex flex-col items-center justify-center p-8">
             <div className="max-h-[90%] relative flex flex-col items-center justify-center max-w-[80%] w-full">
-                <audio ref={audioRef} onEndedCapture={autoPlay} src={src[0] + source} className="max-h-full"/>
-                <div className="flex flex-col w-full border-4 border-red-400 bg-gray-900" ref={controlsRef}>
-                    <div className="text-white text-2xl font-bold p-2 max-w-[95%] overflow-x-auto whitespace-nowrap">{source}</div>
-                    <input type="range" ref={seekerRef} step={1} min={0} max={100} onChange={seek} id="videoSlider"/>
-                    <div className="flex w-full bg-gray-900">
-                        <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/prev.png"/>} onclick={prev}/>
-                        <PlayerButton text={<img className="h-[24px]" src={playing ? "../src/assets/playerAssets/pause.png" : "../src/assets/playerAssets/play.png"}/>} onclick={play}/>
-                        <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/next.png"/>} onclick={next}/>
-                        <div className="flex items-center justify-center text-white py-1 max-w-24 w-full bg-gray-900 font-bold">
-                            <div>{Math.floor(current / 60) + ":" + ("0" + Math.floor(current % 60)).slice(-2)}</div>
-                            <div>/{Math.floor(duration / 60) + ":" + ("0" + Math.floor(duration % 60)).slice(-2)}</div>
-                        </div>
-                        <div className="flex items-center justify-center">
-                            <PlayerButton text={<img className="h-[24px]" src={isMuted ? "../src/assets/playerAssets/muted.png" : "../src/assets/playerAssets/volume.png"}/>} onclick={mute}/>
-                            <input type="range" ref={volumeRef} step={0.01} min={0} max={1} onChange={changeAudio} id="audioSlider"/>
+                <audio ref={audioRef} onEnded={autoPlay} src={src[0] + source} onLoadedMetadata={() => setMetadata(true)} className="max-h-full"/>
+                {metadata &&
+                    <div className="flex flex-col w-full border-4 border-red-400 bg-gray-900" ref={controlsRef}>
+                        <div className="text-white text-2xl font-bold p-2 max-w-[95%] overflow-x-auto whitespace-nowrap">{source}</div>
+                        <input type="range" ref={seekerRef} defaultValue={0} step={1} min={0} max={100} onChange={seek} id="videoSlider"/>
+                        <div className="flex w-full bg-gray-900">
+                            <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/prev.png"/>} onclick={prev}/>
+                            <PlayerButton text={<img className="h-[24px]" src={isPlaying ? "../src/assets/playerAssets/pause.png" : "../src/assets/playerAssets/play.png"}/>} onclick={play}/>
+                            <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/next.png"/>} onclick={next}/>
+                            <div className="flex items-center justify-center text-white py-1 max-w-24 w-full bg-gray-900 font-bold">
+                                <div>{Math.floor(currentTime / 60) + ":" + ("0" + Math.floor(currentTime % 60)).slice(-2)}</div>
+                                <div>/{Math.floor(audioRef.current.duration / 60) + ":" + ("0" + Math.floor(audioRef.current.duration % 60)).slice(-2)}</div>
+                            </div>
+                            <div className="flex items-center justify-center">
+                                <PlayerButton text={<img className="h-[24px]" src={isMuted ? "../src/assets/playerAssets/muted.png" : "../src/assets/playerAssets/volume.png"}/>} onclick={mute}/>
+                                <input type="range" ref={volumeRef} defaultValue={0.3} step={0.01} min={0} max={1} onChange={changeAudio} id="audioSlider"/>
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
             </div>
         </section>
     );
--- src/components/players/videoPlayer.jsx
+++ src/components/players/videoPlayer.jsx
@@ -10,27 +10,25 @@ export function VideoPlayer({ src }) {
     const intervalRef = useRef();
     const timeoutRef = useRef();
 
-    const [playing, setPlaying] = useState(false);
-    const [isFullscreen, setIsFullscreen] = useState(false);
-    const [duration, setDuration] = useState(0);
-    const [current, setCurrent] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
     const [isMuted, setIsMuted] = useState(false);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    const [currentTime, setCurrentTime] = useState(Number);
+
     const [source, setSource] = useState(src[1]);
-
-
+    const [metadata, setMetadata] = useState(false);
  
     function play() {
         if(videoRef.current.paused) {
             videoRef.current.play();
-            setDuration(videoRef.current.duration);
-            setPlaying(true);
+            setIsPlaying(true);
 
             intervalRef.current = setInterval(() => {
                 updateTime();
-            }, 150);
+            }, 100);
         } else {
             videoRef.current.pause();
-            setPlaying(false);
+            setIsPlaying(false);
             clearInterval(intervalRef.current);
         };
     };
@@ -47,12 +45,12 @@ export function VideoPlayer({ src }) {
 
     function updateTime() {
         seekerRef.current.value = (videoRef.current.currentTime / videoRef.current.duration) * 100;
-        setCurrent(videoRef.current.currentTime);
+        setCurrentTime(videoRef.current.currentTime);
     };
 
     function seek() {
         videoRef.current.currentTime = videoRef.current.duration * (seekerRef.current.value / 100);
-        setCurrent(videoRef.current.currentTime);
+        setCurrentTime(videoRef.current.currentTime);
     };
 
     function changeAudio() {
@@ -83,27 +81,29 @@ export function VideoPlayer({ src }) {
 
     function next() {
         if((src[2].indexOf(source) + 1) < src[2].length) {
-            clearInterval(intervalRef.current);
-            setPlaying(false);
             setSource(src[2][src[2].indexOf(source) + 1]);
+            clearInterval(intervalRef.current);
+            setIsPlaying(false);
+            setMetadata(false);
+            setCurrentTime(0);
+            videoRef.current.pause();
             seekerRef.current.value = 0;
-            setCurrent(0);
         };
     };
 
     function prev() {
         if((src[2].indexOf(source) - 1) >= 0) {
-            clearInterval(intervalRef.current);
-            setPlaying(false);
             setSource(src[2][src[2].indexOf(source) - 1]);
+            clearInterval(intervalRef.current);
+            setIsPlaying(false);
+            setMetadata(false);
+            setCurrentTime(0);
+            videoRef.current.pause();
             seekerRef.current.value = 0;
-            setCurrent(0);
         };
     };
 
     useEffect(() => {
-        seekerRef.current.value = 0;
-        audioRef.current.value = 0.5;
         return () =>  {
             clearInterval(intervalRef.current);
             clearTimeout(timeoutRef.current);
@@ -113,29 +113,31 @@ export function VideoPlayer({ src }) {
     return (
         <section className="h-screen w-full flex flex-col items-center justify-center p-8">
             <div className="max-h-[90%] relative flex flex-col items-center justify-center" ref={fullscreenRef} onMouseMove={displayControls}>
-                <video ref={videoRef} onEnded={() => {clearInterval(intervalRef.current); setPlaying(false);}} onClick={play} src={src[0] + source} className="max-h-full"/>
-                <div ref={controlsRef} className="w-full overflow-auto">
-                    <div className="w-full absolute top-0 text-white text-2xl font-bold m-2 max-w-[95%] overflow-x-auto whitespace-nowrap">{source}</div>
-                    <div className="flex flex-col w-full absolute bottom-0">
-                        <input type="range" ref={seekerRef} step={1} min={0} max={100} onChange={seek} id="videoSlider"/>
-                        <div className="flex justify-between">
-                            <div className="flex w-full bg-gray-900">
-                                <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/prev.png"/>} onclick={prev}/>
-                                <PlayerButton text={<img className="h-[24px]" src={playing ? "../src/assets/playerAssets/pause.png" : "../src/assets/playerAssets/play.png"}/>} onclick={play}/>
-                                <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/next.png"/>} onclick={next}/>
-                                <div className="flex items-center justify-center text-white py-1 max-w-24 w-full bg-gray-900 font-bold">
-                                    <div>{Math.floor(current / 60) + ":" + ("0" + Math.floor(current % 60)).slice(-2)}</div>
-                                    <div>/{Math.floor(duration / 60) + ":" + ("0" + Math.floor(duration % 60)).slice(-2)}</div>
+                <video ref={videoRef} onEnded={() => {clearInterval(intervalRef.current); setIsPlaying(false);}} onLoadedMetadata={() => setMetadata(true)} onClick={play} src={src[0] + source} className="max-h-full"/>
+                {metadata && 
+                    <div ref={controlsRef} className="w-full overflow-auto">
+                        <div className="w-full absolute top-0 text-white text-2xl font-bold m-2 max-w-[95%] overflow-x-auto whitespace-nowrap">{source}</div>
+                        <div className="flex flex-col w-full absolute bottom-0">
+                            <input type="range" ref={seekerRef} defaultValue={0} step={1} min={0} max={100} onChange={seek} id="videoSlider"/>
+                            <div className="flex justify-between">
+                                <div className="flex w-full bg-gray-900">
+                                    <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/prev.png"/>} onclick={prev}/>
+                                    <PlayerButton text={<img className="h-[24px]" src={isPlaying ? "../src/assets/playerAssets/pause.png" : "../src/assets/playerAssets/play.png"}/>} onclick={play}/>
+                                    <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/next.png"/>} onclick={next}/>
+                                    <div className="flex items-center justify-center text-white py-1 max-w-24 w-full bg-gray-900 font-bold">
+                                        <div>{Math.floor(currentTime / 60) + ":" + ("0" + Math.floor(currentTime % 60)).slice(-2)}</div>
+                                        <div>/{Math.floor(videoRef.current.duration / 60) + ":" + ("0" + Math.floor(videoRef.current.duration % 60)).slice(-2)}</div>
+                                    </div>
+                                    <div className="flex items-center justify-center">
+                                        <PlayerButton text={<img className="h-[24px]" src={isMuted ? "../src/assets/playerAssets/muted.png": "../src/assets/playerAssets/volume.png"}/>} onclick={mute}/>
+                                        <input type="range" ref={audioRef} defaultValue={0.3} step={0.01} min={0} max={1} onChange={changeAudio} id="audioSlider"/>
+                                    </div>
                                 </div>
-                                <div className="flex items-center justify-center">
-                                    <PlayerButton text={<img className="h-[24px]" src={isMuted ? "../src/assets/playerAssets/muted.png": "../src/assets/playerAssets/volume.png"}/>} onclick={mute}/>
-                                    <input type="range" ref={audioRef} step={0.01} min={0} max={1} onChange={changeAudio} id="audioSlider"/>
-                                </div>
+                                <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/fullscreen.png"/>} onclick={toggleFullscreen}/>
                             </div>
-                            <PlayerButton text={<img className="h-[24px]" src="../src/assets/playerAssets/fullscreen.png"/>} onclick={toggleFullscreen}/>
                         </div>
                     </div>
-                </div>
+                }
             </div>
         </section>
     );
